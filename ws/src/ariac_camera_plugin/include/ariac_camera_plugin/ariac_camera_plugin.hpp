036d1671e21d89022553370d639c947fe3a15de0
Sparsh Bhogavilli <bsparshbss@gmail.com>
2024-03-27 13:15:28 -0400

    latest repo

--- ws/src/ariac_camera_plugin/include/ariac_camera_plugin/ariac_camera_plugin.hpp
+++ ws/src/ariac_camera_plugin/include/ariac_camera_plugin/ariac_camera_plugin.hpp
@@ -1,14 +1,27 @@
 #ifndef ARIAC_CAMERA_PLUGIN_HPP
 #define ARIAC_CAMERA_PLUGIN_HPP
 
+// #include <rclcpp/rclcpp.hpp>
+
 #include <gz/sim/System.hh>
 #include <gz/sim/EntityComponentManager.hh>
+#include <gz/sim/EventManager.hh>
+#include <gz/sim/components/Component.hh>
+#include <gz/sim/components/Name.hh>
+#include <gz/sim/Sensor.hh>
+
+
+#include <gz/rendering/Camera.hh>
+#include <gz/sensors/CameraSensor.hh>
+#include <gz/sensors/Manager.hh>
+
+#include <gz/transport/Node.hh>
 
 namespace ariac_sensors{
   class AriacCameraPlugin : 
     public gz::sim::System,
-    public gz::sim::ISystemConfigure,
-    public gz::sim::ISystemPostUpdate
+    public gz::sim::ISystemConfigure
+    // public gz::sim::ISystemPostUpdate
   {
     public:
       AriacCameraPlugin();
@@ -17,10 +30,21 @@ namespace ariac_sensors{
       void Configure (const gz::sim::Entity &_entity,
                       const std::shared_ptr<const sdf::Element> &_sdf,
                       gz::sim::EntityComponentManager &_ecm,
-                      gz::sim::EventManager &) override;
+                      gz::sim::EventManager &_event_mgr) override;
 
-      void PostUpdate (const gz::sim::UpdateInfo &_info,
-                       const gz::sim::EntityComponentManager &_ecm) override;
+      void OnNewImageFrame(const unsigned char *_image,
+                  unsigned int _width, unsigned int _height,
+                  unsigned int _depth, const std::string &_format);
+      
+      // void PostUpdate (const gz::sim::UpdateInfo &_info,
+      //                  const gz::sim::EntityComponentManager &_ecm) override;
+    
+    private:
+      std::shared_ptr<gz::transport::Node> gz_node_;
+      // rclcpp::Node::SharedPtr ros_node_;
+      gz::rendering::CameraPtr camera_;
+      // gz::common::ConnectionPtr newImageFrameConnection_;
+      std::shared_ptr<gz::sensors::CameraSensor> camera_parent_sensor_;
       
   };
 }
--- ws/src/ariac_camera_plugin/src/ariac_camera_plugin.cpp
+++ ws/src/ariac_camera_plugin/src/ariac_camera_plugin.cpp
@@ -1,23 +1,78 @@
 #include <gz/plugin/Register.hh>
+#include <typeinfo>
+
 
 #include "ariac_camera_plugin/ariac_camera_plugin.hpp"
 
 namespace ariac_sensors{
   
-  AriacCameraPlugin::AriacCameraPlugin() {gzmsg << "\n\nHello from Constructor!!\n\n";}
+  AriacCameraPlugin::AriacCameraPlugin() {gzmsg << "\n\nHello from Constructor!!\n\n";
+    // ROS node
+    // this->ros_node_ = rclcpp::Node::make_shared("ros_gz_ariac_camera");
+    // this->ros_node_->declare_parameter("qos", "default");
+
+    // Gazebo node
+    this->gz_node_ = std::make_shared<gz::transport::Node>();
+
+  }
   AriacCameraPlugin::~AriacCameraPlugin() {}
 
   void AriacCameraPlugin::Configure (const gz::sim::Entity &_entity,
                       const std::shared_ptr<const sdf::Element> &_sdf,
                       gz::sim::EntityComponentManager &_ecm,
-                      gz::sim::EventManager &) {
-                        gzmsg << "\n\nHello from Configure\n\n";
-                      }
+                      gz::sim::EventManager &_event_mgr) {
+    
+    gzmsg << "\n\nHello from Configure\n\n";
 
-  void AriacCameraPlugin::PostUpdate (const gz::sim::UpdateInfo &_info,
-                       const gz::sim::EntityComponentManager &_ecm) {
-                        gzmsg << "\n\nHello from PostUpdate\n\n";
-                       }
+    auto entities = _ecm.EntitiesByComponents(gz::sim::components::Name("rgb_camera")
+                                            // , gz::sim::components::Camera()
+                                              );
+    gzmsg << entities.size() << "\n";
+    // std::cout << typeid(entities[0]).name() << '\n';
+    std::cout << std::to_string(entities[0]) << '\n';
+
+    gz::sim::Sensor sensor = gz::sim::Sensor(entities[0]);
+    if (sensor.Topic(_ecm).has_value()) {
+      gzmsg << sensor.Topic(_ecm).value() << std::endl;
+    } else {
+      gzmsg << "No Value\n";
+    }
+
+    std::cout << sensor.Valid(_ecm);
+
+    // auto manager = gz::sensors::Manager();
+    // gz::sensors::Sensor *sensor_ptr = manager.Sensor(entities[0]);
+    // auto is_active = sensor_ptr->IsActive();
+
+    // std::shared_ptr<gz::sensors::Sensor> sensor_shared_ptr(sensor_ptr);
+
+    // std::shared_ptr<gz::sensors::CameraSensor> cam_shared_ptr = 
+    //   std::dynamic_pointer_cast<gz::sensors::CameraSensor>(sensor_shared_ptr);
+
+    // gz::sensors::CameraSensor *cam_sensor_ptr = 
+    //   std::dynamic_pointer_cast<gz::sensors::CameraSensor*>(sensor_ptr);
+
+    // // auto s_ptr = std::make_shared<gz::sensors::CameraSensor>();//sensor_ptr);
+    // std::shared_ptr<gz::sensors::CameraSensor> cam_shared_ptr(cam_sensor_ptr);
+    // this->camera_ = cam_shared_ptr->RenderingCamera();
+
+    // auto is_active = sensor_shared_ptr->IsActive();
+    // std::cout << topic_name+"\n";
+    // this->camera_parent_sensor_ = std::dynamic_pointer_cast<gz::sensors::CameraSensorPtr>(_sensor);
+    // this->camera_parent_sensor_ = std::make_shared<gz::sensors::CameraSensor>(_entity);//entities[0]);
+    // this->camera_ = this->camera_parent_sensor_->RenderingCamera();
+
+    // this->newImageFrameConnection_ = this->camera_->ConnectNewImageFrame(
+    //   std::bind(&AriacCameraPlugin::OnNewImageFrame, this,
+    //     std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,
+    //     std::placeholders::_4, std::placeholders::_5)
+    // );
+  }
+
+  // void AriacCameraPlugin::PostUpdate (const gz::sim::UpdateInfo &_info,
+  //                      const gz::sim::EntityComponentManager &_ecm) {
+  //                       // gzmsg << "\n\nHello from PostUpdate\n\n";
+  //                      }
 
 
 }
@@ -25,6 +80,6 @@ namespace ariac_sensors{
 GZ_ADD_PLUGIN(
   ariac_sensors::AriacCameraPlugin,
   gz::sim::System,
-  ariac_sensors::AriacCameraPlugin::ISystemConfigure,
-  ariac_sensors::AriacCameraPlugin::ISystemPostUpdate
+  ariac_sensors::AriacCameraPlugin::ISystemConfigure
+  // ariac_sensors::AriacCameraPlugin::ISystemPostUpdate
 )
